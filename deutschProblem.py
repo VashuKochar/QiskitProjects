ad8951381c8e4d64934df165f495f2431f9760bf
VashuKochar <vashukochar@gmail.com>
2022-08-31 18:18:16 +0530

    Problem description before code

--- deutschProblem.py
+++ deutschProblem.py
@@ -1,3 +1,18 @@
+"""
+### Source
+https://learn.qiskit.org/course/ch-gates/phase-kickback
+
+### Problem statement
+Make a function, deutsch() that takes a Deutsch function as a QuantumCircuit and uses the Deutsch algorithm to solve it on a quantum simulator. Your function should return True if the Deutsch funciton is balanced, and False if it's constant.
+
+You can use the function deutsch_problem() to create a QuantumCircuit you can use as input to your deutsch() function.
+
+### References
+QuantumCircuit: https://qiskit.org/documentation/stubs/qiskit.circuit.QuantumCircuit.html
+compose: https://medium.com/arnaldo-gunzi-quantum/how-to-use-the-compose-function-in-qiskit-5983bf4fffcf
+AER Simulator: https://qiskit.org/documentation/tutorials/simulators/1_aer_provider.html
+"""
+
 import numpy as np
 
 # Importing standard Qiskit libraries
